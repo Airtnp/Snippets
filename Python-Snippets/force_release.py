3d41cd8fe0e24a2f4aadad9440d14fb770c987c2
LR.X <xiaolr2003@foxmail.com>
2017-10-05 02:07:49 -0400

    Update force-relase.py

---
+++ Python-Snippets/force_release.py
@@ -0,0 +1,25 @@
+class N():
+  def __init__(self):
+    self.n = 1
+
+class A():
+  def __init__(self):
+    self.a = N()
+    self.b = self.a
+  
+  
+import sys
+import gc
+import ctypes
+t = A()
+
+print(sys.getrefcount(t.a)) # 3
+
+ctypes.pythonapi.Py_DecRef(ctypes.py_object(t.a))
+ctypes.pythonapi.Py_DecRef(ctypes.py_object(t.a))
+
+# Works for simple object
+# ctypes.pythonapi._Py_Dealloc(ctypes.py_object(t.a)) 
+
+# Will collect t.a refcount = 1
+gc.collect()
