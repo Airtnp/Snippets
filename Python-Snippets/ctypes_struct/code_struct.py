8b54a724a655ed17fe5a2a362628693aca12eb78
LR.X <xiaolr2003@foxmail.com>
2017-10-27 20:16:24 -0400

    Update ctypes_base

--- Python-Snippets/ctypes_struct/code_struct.py
+++ Python-Snippets/ctypes_struct/code_struct.py
@@ -1,6 +1,7 @@
 from .ctypes_base import *
-
-
+from .value_struct import *
+from .container_struct import *
+from .string_struct import *
 
 
 class CodeStruct(ctypes.Structure, IObjStruct):
@@ -11,21 +12,54 @@ class CodeStruct(ctypes.Structure, IObjStruct):
         ("co_stacksize"     , c_int),
         ("co_flags"         , c_int),
         ("co_firstlineno"   , c_int),
-        ("co_code"          , py_object), # opcode bytes
-        ("co_consts"        , py_object), # list
-        ("co_names"         , py_object), # list of string
-        ("co_varnames"      , py_object), # tuple of string
-        ("co_freevars"      , py_object), # tuple of string
-        ("co_cellvars"      , py_object), # tuple of string
-        ("co_cell2arg"      , c_voidp),
-        ("co_filename"      , py_object), # unicode
-        ("co_name"          , py_object), # unicode
-        ("co_lnotab"        , py_object), # string
-        ("co_zombieframe"   , c_voidp),
+        ("co_code"          , KPOINTER(BytesStruct)), # opcode bytes
+        ("co_consts"        , POINTER(ListStruct)), # list of string 
+        ("co_names"         , POINTER(ListStruct)), # list of constants
+        ("co_varnames"      , POINTER(TupleStruct)), # tuple of string
+        ("co_freevars"      , POINTER(TupleStruct)), # tuple of string
+        ("co_cellvars"      , POINTER(TupleStruct)), # tuple of string
+        ("co_cell2arg"      , POINTER(c_longlong)),
+        ("co_filename"      , POINTER(UnicodeStruct)), # unicode
+        ("co_name"          , POINTER(UnicodeStruct)), # unicode
+        ("co_lnotab"        , POINTER(UnicodeStruct)), # string
+        ("co_zombieframe"   , c_void_p),
         ("co_weakreflist"   , py_object),
-        ("co_extra"         , py_object)
+        ("co_extra"         , c_void_p)
     ]
 
     def set(self):
         pass
         
+
+class TryBlockStruct(ctypes.Structure):
+    _fields_ = [
+        ("b_type"   , c_int),
+        ("b_handler", c_int),
+        ("b_level"  , c_int)
+    ]
+
+
+class FrameStruct(ctypes.Structure, IObjStruct):
+    pass 
+
+FrameStruct._fields_ = VarObjStruct._fields_ + [
+        ("f_back"           , POINTER(FrameStruct)),
+        ("f_code"           , POINTER(CodeStruct)),
+        ("f_builtins"       , POINTER(DictStruct)),
+        ("f_globals"        , POINTER(DictStruct)),
+        ("f_valuestack"     , c_void_p), # PyObject**, points after the last local
+        ("f_stacktop"       , c_void_p), # PyObject**
+        ("f_trace"          , py_object),
+        # Generator-only
+        ("f_exc_type"       , py_object),
+        ("f_exc_value"      , py_object),
+        ("f_exc_traceback"  , py_object),
+        ("f_gen"            , py_object),
+        # Generator-only ends
+        ("f_lasti"          , c_int),
+        ("f_lineno"         , c_int),
+        ("f_iblock"         , c_int),
+        ("f_executing"      , c_char),
+        ("f_blockstack"     , TryBlockStruct * 20),
+        ("f_localsplus"     , c_void_p) # locals + stack, dynamically sized
+    ]
--- Python-Snippets/ctypes_struct/ctypes_base.py
+++ Python-Snippets/ctypes_struct/ctypes_base.py
@@ -3,12 +3,53 @@ import sys
 from imp import reload
 from ctypes import *
 
+value_address = {}  # POINTER(ty) will restricted by __slots__, use global instead
+value_offset = {}
+
 class IObjStruct:
     @classmethod
     def from_address_(cls, addr):
         t = cls.from_address(addr) # metaclass creates from_address method
-        t.value_addr = addr 
+        t.base_value_addr = addr
+        t.value_addr = addr
+        value_address[id(t)] = addr
+        value_offset[id(t)] = 0
+        # cls.prepare_addr(cls, t)
         return t
+
+    # Fix LP_Ty from_adddress_ problem
+    @staticmethod
+    def prepare_addr(cls, ins):
+        for i in range(len(cls._fields_)):
+            name, ty = cls._fields_[i]
+            if name in dir(ins) and ty.__name__.find("LP_") != -1:   # builtin attributes
+                not_null = True
+                eval_p = "bool(ins.{})"
+                if eval(eval_p.format(name)):
+                    pass
+
+    @property
+    def value_addr(self):
+        return value_address[id(self)] + value_offset[id(self)]
+
+    @value_addr.setter
+    def value_addr(self, v):
+        value_address[id(self)] = v
+
+    def get(self, name):
+        item = None
+        eval_p = "self.{}".format(name)
+        item = eval(eval_p)
+        value_address[id(item)] = value_address[id(self)]
+        ty = self.__class__
+        ity = item.__class__
+        if ity.__name__.find("LP_") != -1:
+            offset = ty.__dict__[name].offset
+            value_offset[id(item)] = 0
+            value_address[id(item)] = c_ulonglong.from_address(value_address[id(self)] + offset).value
+        else:
+            value_offset[id(item)] = value_offset[id(self)]
+        return item
 
 
 class ObjStruct(ctypes.Structure, IObjStruct):
@@ -26,3 +67,13 @@ class VarObjStruct(ctypes.Structure, IObjStruct):
     ]
     _fields_size_ = ObjStruct._fields_size_ + 8 # 24
 
+def KPOINTER(ty):
+    oldcls = POINTER(ty)
+    old_getitem = oldcls.__getitem__
+    def new_getitem(self, offset):
+        item = old_getitem(self, offset)
+        value_address[id(item)] = value_address[id(self)]
+        value_offset[id(item)] = value_offset[id(self)]
+        return item
+    oldcls.__getitem__ = new_getitem
+    return oldcls
