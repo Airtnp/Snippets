2cc0b18e26c2ed5923eedbbda370e7c01ba12be7
LR.X <xiaolr2003@foxmail.com>
2017-11-09 03:37:24 -0500

    Update python-internal

---
+++ Haskell-Snippets/DTVec.hs
@@ -0,0 +1,2 @@
+-- https://stackoverflow.com/questions/27743472/how-to-convince-ghc-that-type-level-addition-is-commutative-to-implement-depend
+-- https://www.schoolofhaskell.com/user/konn/prove-your-haskell-for-great-safety/dependent-types-in-haskell
--- Python-Snippets/internals/get_varname.py
+++ Python-Snippets/internals/get_varname.py
@@ -139,3 +139,63 @@ def test_all():
     g()
     print(h())
     print(u())
+
+# Error solution: http://wesmckinney.com/blog/python-r-and-the-allure-of-magic/
+import ast
+import inspect
+import sys
+
+def merge(a, b):
+    f, args, _ = parse_stmt(inspect.currentframe().f_back)
+    return (args, (a, b))
+
+def parse_stmt(frame):
+    info = inspect.getframeinfo(frame)
+    call = info[-3][0]
+    mod = ast.parse(call)   # This shall be changed
+    body = mod.body[0]
+    if isinstance(body, (ast.Assign, ast.Expr)):
+        call = body.value
+    elif isinstance(body, ast.Call):
+        call = body
+    return _parse_call(call)
+
+def _parse_call(call):
+    func = _maybe_format_attribute(call.func)
+
+    str_args = []
+    for arg in call.args:
+        if isinstance(arg, ast.Name):
+            str_args.append(arg.id)
+        elif isinstance(arg, ast.Call):
+            formatted = _format_call(arg)
+            str_args.append(formatted)
+
+    return func, str_args, {}
+
+def _format_call(call):
+    func, args, kwds = _parse_call(call)
+    content = ''
+    if args:
+        content += ', '.join(args)
+    if kwds:
+        fmt_kwds = ['%s=%s' % item for item in kwds.iteritems()]
+        joined_kwds = ', '.join(fmt_kwds)
+        if args:
+            content = content + ', ' + joined_kwds
+        else:
+            content += joined_kwds
+    return '%s(%s)' % (func, content)
+
+def _maybe_format_attribute(name):
+    if isinstance(name, ast.Attribute):
+        return _format_attribute(name)
+    return name.id
+
+def _format_attribute(attr):
+    obj = attr.value
+    if isinstance(attr.value, ast.Attribute):
+        obj = _format_attribute(attr.value)
+    else:
+        obj = obj.id
+    return '.'.join((obj, attr.attr))
